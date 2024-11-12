242965a19cf8f7246d3ff00bb029e0ab2bfc53be
LexMari <legosmurf@outlook.com>
2024-11-12 11:01:15 +0000

    Old removed migrations, as well as new one - easier to redo from scratch

---
+++ Server/ApiServer.Infrastructure/Migrations/20241112105855_InitialCreate.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ApiServer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FlashcardSets",
+                columns: table => new
+                {
+                    FlashcardSetId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlashcardSets", x => x.FlashcardSetId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", unicode: false, maxLength: 50, nullable: false),
+                    PasswordHash = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    PasswordSalt = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    IsAdministrator = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                    table.UniqueConstraint("AK_Users_Username", x => x.Username);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FlashCards",
+                columns: table => new
+                {
+                    FlashCardId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FlashcardSetId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Question = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    Answer = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    Difficulty = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlashCards", x => x.FlashCardId);
+                    table.ForeignKey(
+                        name: "FK_FlashCards_FlashcardSets_FlashcardSetId",
+                        column: x => x.FlashcardSetId,
+                        principalTable: "FlashcardSets",
+                        principalColumn: "FlashcardSetId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    CommentId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FlashcardSetId = table.Column<int>(type: "INTEGER", nullable: true),
+                    CommentText = table.Column<string>(type: "TEXT", unicode: false, maxLength: 500, nullable: false),
+                    AuthorId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.CommentId);
+                    table.ForeignKey(
+                        name: "FK_Comments_FlashcardSets_FlashcardSetId",
+                        column: x => x.FlashcardSetId,
+                        principalTable: "FlashcardSets",
+                        principalColumn: "FlashcardSetId");
+                    table.ForeignKey(
+                        name: "FK_Comments_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "IsAdministrator", "PasswordHash", "PasswordSalt", "Username" },
+                values: new object[,]
+                {
+                    { 1, false, "w3zLSWUZ4dLP20icJT593RgHRq7buV0nXKRbf10uxpf8flMhTuegV94NUdUSHHDW0FbinvVAINAaX0B9mjo0dQ==", "cYeT914bXdEvj7TYm4UWuOsICmhUrcsQqP6UXjsy4GKXRVJeG9u93OT5mpQ9UBBcoYl3g7h4icOi50zjTHL+MA==", "student" },
+                    { 2, true, "TXNqvMXo1b5mH7O7/iI8UquPY3XR/2sNNtvk5y/F5SFskW9CaFM0wasY+tP132RGbycLBxIeh1pPLDSzspadgg==", "sapB/Ozr4IJsQflqc/+yfekih1w2eMIqqrvNJ9smmExxRZMB7oGuogMUVkfu7SdB/gk97meNI4WaHCEMzfjyMg==", "admin" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AuthorId",
+                table: "Comments",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_FlashcardSetId",
+                table: "Comments",
+                column: "FlashcardSetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlashCards_FlashcardSetId",
+                table: "FlashCards",
+                column: "FlashcardSetId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "FlashCards");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "FlashcardSets");
+        }
+    }
+}
